fa37af7c1d01655218d20cf8921a456251fbf66e
jhonatan <jhonatannascimentodesousa@gmail.com>
2024-09-19 19:51:48 -0300

    primeiro passo

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+
+function Soma ($x,$y){
+    echo $x + $y;
+    
+}
+
+
+function Subtracao ($x,$b){
+    echo $x - $b;
+
+}
+
+function Divisao ($a,$b){
+    echo $a / $b;
+
+}
+
+function Multiplicacao ($r,$s){
+    echo $r * $s;
+
+}
+function Repeticao (){
+
+    for ($i = 0; $i <= 10; $i++  ){
+        echo $i ."<br>";
+
+    }
+}
+
+Repeticao();
+
+
